dcd6b04737733bd6b9416497390bdc074312d845
花木凋零成兰 <18570354653@163.com>
2024-12-18 23:37:49 +0800

    casual

---
+++ demo-casual/src/main/java/top/ytazwc/casual/DemoCasualApplication.java
@@ -0,0 +1,13 @@
+package top.ytazwc.casual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCasualApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCasualApplication.class, args);
+    }
+
+}
