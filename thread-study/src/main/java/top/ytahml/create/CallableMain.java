c0c3f6b82ff24e5d96c968f7c35c5d2f72c73c40
yangtao <00103943@lesso.com>
2025-05-22 10:45:12 +0800

    创建CallableMain方法

---
+++ thread-study/src/main/java/top/ytahml/create/CallableMain.java
@@ -0,0 +1,15 @@
+package top.ytahml.create;
+
+/**
+ * @author 00103943
+ * @date 2025-05-22 10:44
+ * @package top.ytahml.create
+ * @description 实现 Callable 接口
+ */
+public class CallableMain {
+
+    public static void main(String[] args) {
+
+    }
+
+}
