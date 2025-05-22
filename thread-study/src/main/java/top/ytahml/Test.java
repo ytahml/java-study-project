f01d156bc9a5fa260288ceb72967549468655a12
yangtao <00103943@lesso.com>
2025-05-22 16:45:28 +0800

    创建Test类并创建main方法

---
+++ thread-study/src/main/java/top/ytahml/Test.java
@@ -0,0 +1,15 @@
+package top.ytahml;
+
+/**
+ * @author 00103943
+ * @date 2025-05-22 16:45
+ * @package top.ytahml
+ * @description
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+
+}
