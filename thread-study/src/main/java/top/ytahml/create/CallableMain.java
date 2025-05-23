ef4b5932f444aa7c0e36ef850fb65994a03edc49
yangtao <00103943@lesso.com>
2025-05-23 09:23:14 +0800

    实现Callable接口创建多线程

--- thread-study/src/main/java/top/ytahml/create/CallableMain.java
+++ thread-study/src/main/java/top/ytahml/create/CallableMain.java
@@ -1,5 +1,10 @@
 package top.ytahml.create;
 
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
 /**
  * @author 00103943
  * @date 2025-05-22 10:44
@@ -8,8 +13,22 @@ package top.ytahml.create;
  */
 public class CallableMain {
 
-    public static void main(String[] args) {
-
+    private static class CallableThread implements Callable<Integer> {
+        @Override
+        public Integer call() throws Exception {
+            System.out.println("当前线程: " + Thread.currentThread().getName());
+            System.out.println("使用Callable创建线程 ... ");
+            Random random = new Random();
+            return random.nextInt();
+        }
     }
 
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        CallableThread callableThread = new CallableThread();
+        FutureTask<Integer> futureTask = new FutureTask<>(callableThread);
+        Thread thread = new Thread(futureTask);
+        thread.start();
+        Integer result = futureTask.get();
+        System.out.println(result);
+    }
 }
