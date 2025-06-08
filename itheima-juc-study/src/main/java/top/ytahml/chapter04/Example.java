6e203126d341cf2a72400ba1f109b71d11a15187
花木凋零成兰 <18570354653@163.com>
2025-06-08 16:24:01 +0800

    并发访问共享资源出现问题

---
+++ itheima-juc-study/src/main/java/top/ytahml/chapter04/Example.java
@@ -0,0 +1,43 @@
+package top.ytahml.chapter04;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author 花木凋零成兰
+ * @title Example
+ * @date 2025-06-08 15:39
+ * @package top.ytahml.chapter04
+ * @description 测试并发访问共享变量
+ */
+@Slf4j
+public class Example {
+
+    static int counter = 0;
+
+    @SneakyThrows
+    public static void main(String[] args) {
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 5000; i++) {
+                counter ++;
+            }
+        }, "t1");
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 5000; i++) {
+                counter --;
+            }
+        }, "t2");
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+        log.debug("counter = {}", counter);
+
+
+
+    }
+
+}
