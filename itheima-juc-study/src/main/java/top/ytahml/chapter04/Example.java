b4c16981064fa0b2ce7465b6288bcc9f14d6656d
花木凋零成兰 <18570354653@163.com>
2025-06-08 16:32:51 +0800

    synchronized 对象锁保证临界区代码原子性

--- itheima-juc-study/src/main/java/top/ytahml/chapter04/Example.java
+++ itheima-juc-study/src/main/java/top/ytahml/chapter04/Example.java
@@ -15,18 +15,24 @@ public class Example {
 
     static int counter = 0;
 
+    static final Object LOCK = new Object();
+
     @SneakyThrows
     public static void main(String[] args) {
 
         Thread t1 = new Thread(() -> {
             for (int i = 0; i < 5000; i++) {
-                counter ++;
+                synchronized (LOCK) {
+                    counter ++;
+                }
             }
         }, "t1");
 
         Thread t2 = new Thread(() -> {
             for (int i = 0; i < 5000; i++) {
-                counter --;
+                synchronized (LOCK) {
+                    counter --;
+                }
             }
         }, "t2");
         t1.start();
