465f2d123487eb64d2c22e6c63e0bb64178283d9
花木凋零成兰 <18570354653@163.com>
2025-07-10 21:49:11 +0800

    04_线程状态转换

---
+++ itheima-juc-study/src/main/java/top/ytahml/chapter04/ThreadState.java
@@ -0,0 +1,12 @@
+package top.ytahml.chapter04;
+
+/**
+ * @author 花木凋零成兰
+ * @title ThreadState
+ * @date 2025-07-10 21:01
+ * @package top.ytahml.chapter04
+ * @description
+ */
+public class ThreadState {
+
+}
