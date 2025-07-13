f7e038fada677b5ef5f56e97484140959c6e3f5b
花木凋零成兰 <18570354653@163.com>
2025-07-13 15:28:10 +0800

    04_哲学家就餐问题

---
+++ itheima-juc-study/src/main/java/top/ytahml/chapter04/locks/Chopstick.java
@@ -0,0 +1,23 @@
+package top.ytahml.chapter04.locks;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @author 花木凋零成兰
+ * @title Chopstick
+ * @date 2025-07-13 15:16
+ * @package top.ytahml.chapter04.locks
+ * @description 筷子
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Chopstick {
+
+    private String name;
+
+}
---
+++ itheima-juc-study/src/main/java/top/ytahml/chapter04/locks/Philosopher.java
@@ -0,0 +1,51 @@
+package top.ytahml.chapter04.locks;
+
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author 花木凋零成兰
+ * @title Philosopher
+ * @date 2025-07-13 15:17
+ * @package top.ytahml.chapter04.locks
+ * @description 哲学家
+ */
+@Slf4j
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Philosopher extends Thread {
+
+    Chopstick left;
+
+    Chopstick right;
+
+    public Philosopher(String name, Chopstick left, Chopstick right) {
+        super(name);
+        this.left = left;
+        this.right = right;
+    }
+
+    @SneakyThrows
+    private void eat() {
+        log.debug("eating ...");
+        sleep(1000);
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            // 获得左手筷子
+            synchronized (left) {
+                // 右手筷子
+                synchronized (right) {
+                    eat();
+                }
+                // 放下右手
+            }
+            // 放下左手
+        }
+    }
+}
+
