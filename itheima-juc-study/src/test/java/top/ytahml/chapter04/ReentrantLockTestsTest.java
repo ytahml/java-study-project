6b61744b7792a5cc228c4da32ca2348b8e750ff6
花木凋零成兰 <18570354653@163.com>
2025-07-13 22:26:33 +0800

    04_ReentrantLock锁的可打断

---
+++ itheima-juc-study/src/test/java/top/ytahml/chapter04/ReentrantLockTestsTest.java
@@ -0,0 +1,61 @@
+package top.ytahml.chapter04;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+import static org.junit.Assert.*;
+import static top.ytahml.utils.ThreadUtils.sleep;
+
+/**
+ * @author 花木凋零成兰
+ * @title ReentrantLockTestsTest
+ * @date 2025-07-13 22:19
+ * @package top.ytahml.chapter04
+ * @description
+ */
+@Slf4j
+public class ReentrantLockTestsTest {
+
+    private static final ReentrantLock LOCK = new ReentrantLock();
+
+    // 测试可重入锁，可打断的特性
+    @SneakyThrows
+    @Test
+    public void test1() {
+        // 可以被打断的获取锁
+        Thread t1 = new Thread(() -> {
+            try {
+                log.debug("尝试获取到锁 ... ");
+                // 如果没有竞争，则获取到LOCK对象锁
+                // 如果有竞争，则进入阻塞队列，可以被其他线程用 interrupt 方法打断
+                LOCK.lockInterruptibly();
+            } catch (InterruptedException e) {
+                log.debug("没有获取到锁: {}", e.getMessage());
+                return;
+            }
+            try {
+                log.debug("获取到锁 ...");
+            } finally {
+                LOCK.unlock();
+            }
+        }, "t1");
+
+        // 主线程先获取到锁
+        LOCK.lock();
+
+        t1.start();
+
+        // 打断 t1
+        sleep(2000);
+        log.debug("打断t1 ...");
+        t1.interrupt();
+
+
+        sleep(2000);
+
+    }
+
+}
