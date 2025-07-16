42466a72a58b41bf63ec7a2dca8290d53fe62163
花木凋零成兰 <18570354653@163.com>
2025-07-16 21:20:31 +0800

    04_交替输出-使用ReentrantLock实现

--- itheima-juc-study/src/main/java/top/ytahml/chapter04/AlternateOutput.java
+++ itheima-juc-study/src/main/java/top/ytahml/chapter04/AlternateOutput.java
@@ -2,7 +2,12 @@ package top.ytahml.chapter04;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.extern.slf4j.Slf4j;
+import top.ytahml.utils.ThreadUtils;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @author 花木凋零成兰
@@ -15,10 +20,58 @@ import lombok.extern.slf4j.Slf4j;
 public class AlternateOutput {
 
     public static void main(String[] args) {
-        WaitNotify waitNotify = new WaitNotify(1, 5);
-        new Thread(() -> waitNotify.print("a", 1, 2), "a").start();
-        new Thread(() -> waitNotify.print("b", 2, 3), "b").start();
-        new Thread(() -> waitNotify.print("c", 3, 1), "b").start();
+//        WaitNotify waitNotify = new WaitNotify(1, 5);
+//        new Thread(() -> waitNotify.print("a", 1, 2), "a").start();
+//        new Thread(() -> waitNotify.print("b", 2, 3), "b").start();
+//        new Thread(() -> waitNotify.print("c", 3, 1), "b").start();
+
+        AwaitSignal awaitSignal = new AwaitSignal(5);
+        Condition a = awaitSignal.newCondition();
+        Condition b = awaitSignal.newCondition();
+        Condition c = awaitSignal.newCondition();
+        new Thread(() -> awaitSignal.print("a", a, b), "a").start();
+        new Thread(() -> awaitSignal.print("b", b, c), "b").start();
+        new Thread(() -> awaitSignal.print("c", c, a), "b").start();
+        // 先唤醒a
+        ThreadUtils.sleep(100);
+        awaitSignal.lock();
+        try {
+            log.debug("开始打印");
+            a.signal();
+        } finally {
+            awaitSignal.unlock();
+        }
+    }
+
+    @Data
+    @EqualsAndHashCode(callSuper = true)
+    private static class AwaitSignal extends ReentrantLock {
+        private int loopNumber;
+
+        public AwaitSignal(int loopNumber) {
+            this.loopNumber = loopNumber;
+        }
+
+        /**
+         * 循环打印
+         * @param str 打印内容
+         * @param current 条件
+         * @param next 下一个满足的条件
+         */
+        public void print(String str, Condition current, Condition next) {
+            for (int i = 0; i < loopNumber; i++) {
+                lock();
+                try {
+                    current.await();
+                    System.out.println(str);
+                    next.signal();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    unlock();
+                }
+            }
+        }
     }
 
     @Data
