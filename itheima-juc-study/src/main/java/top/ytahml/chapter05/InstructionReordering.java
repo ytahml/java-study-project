22776acaa01c3ef54283d117130a30bc7d387896
花木凋零成兰 <18570354653@163.com>
2025-08-10 20:32:19 +0800

    05_指令重排使用JCStress测试

---
+++ itheima-juc-study/src/main/java/top/ytahml/chapter05/InstructionReordering.java
@@ -0,0 +1,37 @@
+package top.ytahml.chapter05;
+
+import org.openjdk.jcstress.annotations.*;
+import org.openjdk.jcstress.infra.results.I_Result;
+
+/**
+ * @author 花木凋零成兰
+ * @title InstructionReordering
+ * @date 2025-08-10 16:29
+ * @package top.ytahml.chapter05
+ * @description 指令重排测试
+ * 采用jcstress框架进行测试
+ */
+@JCStressTest
+@Outcome(id = {"1", "4"}, expect = Expect.ACCEPTABLE, desc = "ok")
+@Outcome(id = "0", expect = Expect.ACCEPTABLE_INTERESTING, desc = "!!!!")
+@State
+public class InstructionReordering {
+
+    int num = 0;
+    boolean ready = false;
+
+    @Actor
+    public void actor1(I_Result r) {
+        if (ready) {
+            r.r1 = num + num;
+        } else {
+            r.r1 = 1;
+        }
+    }
+    @Actor
+    public void actor2(I_Result r) {
+        num = 2;
+        ready = true;
+    }
+
+}
