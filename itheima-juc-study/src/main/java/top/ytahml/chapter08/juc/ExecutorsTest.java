e5fc651d8646fa1aac5a2b43aeb98ac0a291e7f8
imulan <18570354653@163.com>
2025-10-01 10:41:19 +0800

    08_使用Executors创建固定大小线程池+自定义线程工厂

--- itheima-juc-study/src/main/java/top/ytahml/chapter08/juc/ExecutorsTest.java
+++ itheima-juc-study/src/main/java/top/ytahml/chapter08/juc/ExecutorsTest.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * JDK 提供的线程池工厂工具 Executors
@@ -16,7 +18,13 @@ public class ExecutorsTest {
 
     public static void main(String[] args) {
         // 固定大小的线程池
-        ExecutorService newFixedThreadPool = Executors.newFixedThreadPool(2);
+        ExecutorService newFixedThreadPool = Executors.newFixedThreadPool(2, new ThreadFactory() {
+            private AtomicInteger t = new AtomicInteger(1);
+            @Override
+            public Thread newThread(Runnable r) {
+                return new Thread(r, "imulan-pool-" + t.getAndIncrement());
+            }
+        });
         for (int i = 0; i < 10; i++) {
             int finalI = i;
             newFixedThreadPool.execute(() -> {
