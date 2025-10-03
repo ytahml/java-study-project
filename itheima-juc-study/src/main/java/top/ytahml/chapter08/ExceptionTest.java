b63a5bbef5f09c4f64a825c901594205155323c8
imulan <18570354653@163.com>
2025-10-03 13:46:32 +0800

    08_如何处理线程池异常

---
+++ itheima-juc-study/src/main/java/top/ytahml/chapter08/ExceptionTest.java
@@ -0,0 +1,12 @@
+package top.ytahml.chapter08;
+
+/**
+ * 如何处理线程池异常：
+ * 1、手动 try catch
+ * 2、使用 Future 来获取任务执行返回结果，出现异常时 get() 会自动打印异常信息
+ *
+ * @author 花木凋零成兰
+ * @since 2025/10/3 下午1:44
+ */
+public class ExceptionTest {
+}
