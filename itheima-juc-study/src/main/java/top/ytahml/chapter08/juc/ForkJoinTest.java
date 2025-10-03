96b45965c0f1dc04e09ba000ddefd4f463119e51
imulan <18570354653@163.com>
2025-10-03 15:23:33 +0800

    08_ForkJoin线程池，求1~n的整数和（优化拆分）

--- itheima-juc-study/src/main/java/top/ytahml/chapter08/juc/ForkJoinTest.java
+++ itheima-juc-study/src/main/java/top/ytahml/chapter08/juc/ForkJoinTest.java
@@ -3,6 +3,7 @@ package top.ytahml.chapter08.juc;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.SneakyThrows;
+import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.ForkJoinPool;
@@ -19,7 +20,8 @@ public class ForkJoinTest {
 
     public static void main(String[] args) {
         ForkJoinPool pool = new ForkJoinPool(4);
-        System.out.println(pool.invoke(new MyTask(5)));
+//        System.out.println(pool.invoke(new MyTask(5)));
+        System.out.println(pool.invoke(new AddTask(1, 5)));
 
     }
 
@@ -45,6 +47,43 @@ public class ForkJoinTest {
             // 让一个线程继续执行子任务
             ForkJoinTask<Integer> fork = new MyTask(n - 1).fork();
             return n + fork.join();
+        }
+    }
+
+    @ToString
+    @Slf4j
+    private static class AddTask extends RecursiveTask<Integer> {
+
+        int begin;
+        int end;
+
+        public AddTask(int begin, int end) {
+            this.begin = begin;
+            this.end = end;
+        }
+
+        @Override
+        protected Integer compute() {
+            if (begin == end) {
+                log.debug("join: {}", begin);
+                return begin;
+            }
+            if (end - begin == 1) {
+                log.debug("join: {} + {} = {}", begin, end, begin + end);
+                return begin + end;
+            }
+
+
+            // 根据中间值，分别求和
+            int mid = begin + (end - begin) / 2;
+            AddTask t1 = new AddTask(begin, mid);
+            t1.fork();
+            AddTask t2 = new AddTask(mid + 1, end);
+            t2.fork();
+            log.debug("fork: {} + {} = ?", t1, t2);
+            int result = t1.join() + t2.join();
+            log.debug("join: {} + {} = {}", t1, t2, result);
+            return result;
         }
     }
 
