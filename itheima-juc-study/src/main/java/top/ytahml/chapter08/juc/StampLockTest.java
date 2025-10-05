71a14ae2daaa15716f9475b0eaa19f523ad2677a
imulan <18570354653@163.com>
2025-10-05 13:20:23 +0800

    08_StampLock读写锁-读读并发

---
+++ itheima-juc-study/src/main/java/top/ytahml/chapter08/juc/StampLockTest.java
@@ -0,0 +1,78 @@
+package top.ytahml.chapter08.juc;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import top.ytahml.utils.ThreadUtils;
+
+import java.util.concurrent.locks.StampedLock;
+
+/**
+ * StampLock读写锁，性能更好，需要配合戳(stamp)使用
+ * 读读并发
+ * @author 花木凋零成兰
+ * @since 2025/10/5 下午1:03
+ */
+@Slf4j
+public class StampLockTest {
+
+    public static void main(String[] args) {
+        DataContainerStamped container = new DataContainerStamped(1);
+        new Thread(() -> {
+            System.out.println(container.read(1000));
+        }, "t1").start();
+        ThreadUtils.sleep(500);
+        new Thread(() -> {
+            System.out.println(container.read(0));
+        }, "t2").start();
+    }
+
+    @Slf4j
+    @Getter
+    private static class DataContainerStamped {
+
+        private int data;
+        private final StampedLock lock = new StampedLock();
+
+        public DataContainerStamped(int data) {
+            this.data = data;
+        }
+
+        // 入参：读取将来耗费的时间
+        private int read(int readTime) {
+            long stamp = lock.tryOptimisticRead();
+            log.info("optimistic read lock {}", stamp);
+            ThreadUtils.sleep(readTime);
+            if (lock.validate(stamp)) {
+                log.debug("read finish {}", stamp);
+                return data;
+            }
+            // 锁升级，从乐观读锁升级为读锁
+            log.warn("rank up read lock: {}", stamp);
+            try {
+                stamp = lock.readLock();
+                log.debug("read data: {}", data);
+                ThreadUtils.sleep(readTime);
+                log.debug("read finish ...");
+                return data;
+            } finally {
+                log.info("read unlock {}", stamp);
+                lock.unlockRead(stamp);
+            }
+        }
+
+        public void write(int newData) {
+            long stamp = lock.writeLock();
+            log.info("write lock {}", stamp);
+            try {
+                log.debug("write data: {}", newData);
+                ThreadUtils.sleep(2000);
+                this.data = newData;
+            } finally {
+                log.info("write unlock {}", stamp);
+                lock.unlock(stamp);
+            }
+        }
+
+    }
+
+}
