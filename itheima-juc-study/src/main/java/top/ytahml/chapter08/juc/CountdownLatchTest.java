a33dc3d253b6a3986802f8a2058c87ea2efe2ad9
imulan <18570354653@163.com>
2025-10-12 20:38:27 +0800

    08_countdownLatch 的应用，多个线程等待加载

--- itheima-juc-study/src/main/java/top/ytahml/chapter08/juc/CountdownLatchTest.java
+++ itheima-juc-study/src/main/java/top/ytahml/chapter08/juc/CountdownLatchTest.java
@@ -4,6 +4,8 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import top.ytahml.utils.ThreadUtils;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -19,7 +21,7 @@ public class CountdownLatchTest {
 
     @SneakyThrows
     public static void main(String[] args) {
-        CountDownLatch latch = new CountDownLatch(3);
+//        CountDownLatch latch = new CountDownLatch(3);
 /*
         new Thread(() -> {
             log.debug("t1 start ...");
@@ -48,37 +50,66 @@ public class CountdownLatchTest {
         */
 
         // 使用线程池改进
-        ExecutorService service = Executors.newFixedThreadPool(4);
-        service.submit(() -> {
-            log.debug("start ...");
-            ThreadUtils.sleep(2000);
-            latch.countDown();
-            log.debug("end ...");
-        });
-        service.submit(() -> {
-            log.debug("start ...");
-            ThreadUtils.sleep(1500);
-            latch.countDown();
-            log.debug("end ...");
-        });
-        service.submit(() -> {
-            log.debug("start ...");
-            ThreadUtils.sleep(500);
-            latch.countDown();
-            log.debug("end ...");
-        });
+//        ExecutorService service = Executors.newFixedThreadPool(4);
+//        service.submit(() -> {
+//            log.debug("start ...");
+//            ThreadUtils.sleep(2000);
+//            latch.countDown();
+//            log.debug("end ...");
+//        });
+//        service.submit(() -> {
+//            log.debug("start ...");
+//            ThreadUtils.sleep(1500);
+//            latch.countDown();
+//            log.debug("end ...");
+//        });
+//        service.submit(() -> {
+//            log.debug("start ...");
+//            ThreadUtils.sleep(500);
+//            latch.countDown();
+//            log.debug("end ...");
+//        });
+//
+//        service.submit(() -> {
+//            log.info("waiting ... ");
+//            try {
+//                latch.await();
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//            log.warn("waited ...");
+//        });
 
-        service.submit(() -> {
-            log.info("waiting ... ");
-            try {
-                latch.await();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            log.warn("waited ...");
-        });
+        // 等待多线程准备完毕
+        ExecutorService service = Executors.newFixedThreadPool(10);
+        // 控制等待
+        CountDownLatch latch = new CountDownLatch(10);
+        // 存储加载结果
+        String[] all = new String[10];
+        // 随机休眠时间
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            int finalI = i;
+            service.submit(() -> {
+                executor(random, all, finalI);
+                latch.countDown();
+            });
+        }
+        latch.await();
+        System.out.print("\n" + "游戏开始");
 
+        // 手动结束线程池
+        service.shutdown();
+    }
 
+    private static void executor(Random random, String[] all, int index) {
+        for (int i = 0; i <= 100; i++) {
+            // 模拟用户加载过程，随机睡眠
+            ThreadUtils.sleep(random.nextInt(100));
+            all[index] = i + "%";
+            // 去掉换行，通过回车符覆盖原先打印结果
+            System.out.print("\r" + Arrays.toString(all));
+        }
     }
 
 }
